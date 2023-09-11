1c4d25524d373c2afa428ced97559026396902a5
kelvin <knjuiri@gmail.com>
2023-09-11 19:22:26 +0300

    update

--- src/Components/Skills/skills.jsx
+++ src/Components/Skills/skills.jsx
@@ -1,19 +1,21 @@
-import React from 'react';
+import React from "react";
 import "./skills.css";
-import Frontend from './Frontend';
-import Backend from './Backend';
+import Frontend from "./Frontend";
+import Backend from "./Backend";
 
 const skills = () => {
   return (
     <section className="skills section" id="skills">
-         <h2 className="section_title">Skills</h2>
-      <h4 className="section_subtitle">My <span>Technical </span>Level</h4>
+      <h2 className="section_title">Skills</h2>
+      <h4 className="section_subtitle">
+        My <span>Technical </span>Level
+      </h4>
       <div className="skills_container container grid">
-            <Frontend/>
-            <Backend/>
+        <Frontend />
+        <Backend />
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default skills
+export default skills;
