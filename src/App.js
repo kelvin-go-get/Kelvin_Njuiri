e69c737b14e4933d67553fcf05b1b3a283747ed8
kelvin <knjuiri@gmail.com>
2023-09-22 14:17:52 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Main from './Components/Main/main';
+// import Main from './Components/Main/main';
 import Header from './Components/Header/Header';
 import Home from './Components/Home/Home'
 import About from './Components/About/about';
@@ -11,7 +11,7 @@ import Services from './Components/Services/services';
 const App = () => {
   return (
     <>
-    <Main/>
+    {/* <Main/> */}
     <Header/>
     <main className='main'>
     <Home/>
--- src/Components/Main/main.jsx
+++ src/Components/Main/main.jsx
@@ -1,15 +1,15 @@
-import React from "react";
-// Correct the import path
-import "./main.css";
+// import React from "react";
+// // Correct the import path
+// import "./main.css";
 
-const main = () => {
-  return (
-    <div className="background-container">
-      <video autoPlay muted loop id="myVideo">
-        <source src="/back2.mp4" type="video/mp4" />
-      </video>
-    </div>
-  );
-};
+// const main = () => {
+//   return (
+//     <div className="background-container">
+//       <video autoPlay muted loop id="myVideo">
+//         <source src="/back2.mp4" type="video/mp4" />
+//       </video>
+//     </div>
+//   );
+// };
 
-export default main;
+// export default main;
