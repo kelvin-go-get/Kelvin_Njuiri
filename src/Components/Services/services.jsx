37ba402b95236a50d0106560e4064ca5297921c3
kelvin <knjuiri@gmail.com>
2023-09-23 23:47:58 +0300

    portfolio update

--- src/Components/Services/services.jsx
+++ src/Components/Services/services.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
-import "./services.css"
+import "./services.css";
 
 const Services = () => {
-    const [toggleState, setToggleState] = useState(0);
+  const [toggleState, setToggleState] = useState(0);
 
-    const toggleTab = (index) => {
-        setToggleState(index);
-    }
+  const toggleTab = (index) => {
+    setToggleState(index);
+  };
   return (
     <section className="services section" id="services">
       <h2 className="section_title">Services</h2>
@@ -17,7 +17,10 @@ const Services = () => {
         <div className="services_content">
           <div>
             <i className="uil uil-web-grid services_icon"></i>
-            <h3 className="services_title"><span>Website</span> <br/>Responsiveness</h3>
+            <h3 className="services_title">
+              <span>Website</span> <br />
+              Responsiveness
+            </h3>
           </div>
 
           <span className="services_button" onClick={() => toggleTab(1)}>
@@ -25,14 +28,25 @@ const Services = () => {
             <i className="uil uil-arrow-right services_button-icon"></i>
           </span>
 
-          <div className={toggleState === 1 ? "services_modal active-modal" : "services_modal"}>
+          <div
+            className={
+              toggleState === 1
+                ? "services_modal active-modal"
+                : "services_modal"
+            }
+          >
             <div className="services_modal-content">
-              <i onClick={() => toggleTab(0)} className="uil uil-times services_modal-close"></i>
+              <i
+                onClick={() => toggleTab(0)}
+                className="uil uil-times services_modal-close"
+              ></i>
 
-              <h3 className="services_modal-title">Website <span>Responsiveness</span></h3>
+              <h3 className="services_modal-title">
+                Website <span>Responsiveness</span>
+              </h3>
               <p className="services_modal-description">
-                <span>2 years</span> of experienced web service, delivering exceptional quality 
-                to clients and companies.
+                <span>1 year</span> of experienced web service, delivering
+                exceptional quality to clients and companies.
               </p>
 
               <ul className="services_modal-services grid">
@@ -79,7 +93,9 @@ const Services = () => {
         <div className="services_content">
           <div>
             <i className="uil uil-arrow services_icon"></i>
-            <h3 className="services_title"><span>Ui/Ux</span> <br/> Designer</h3>
+            <h3 className="services_title">
+              <span>Ui/Ux</span> <br /> Designer
+            </h3>
           </div>
 
           <span className="services_button" onClick={() => toggleTab(2)}>
@@ -87,14 +103,25 @@ const Services = () => {
             <i className="uil uil-arrow-right services_button-icon"></i>
           </span>
 
-          <div className={toggleState === 2 ? "services_modal active-modal" : "services_modal"}>
+          <div
+            className={
+              toggleState === 2
+                ? "services_modal active-modal"
+                : "services_modal"
+            }
+          >
             <div className="services_modal-content">
-              <i onClick={() => toggleTab(0)} className="uil uil-times services_modal-close"></i>
+              <i
+                onClick={() => toggleTab(0)}
+                className="uil uil-times services_modal-close"
+              ></i>
 
-              <h3 className="services_modal-title">Ui/Ux <span>Designer</span></h3>
+              <h3 className="services_modal-title">
+                Ui/Ux <span>Designer</span>
+              </h3>
               <p className="services_modal-description">
-                <span>2 years</span> of experienced service, delivering exceptional quality
-                to clients and companies.
+                <span>1 year</span> of experienced service, delivering
+                exceptional quality to clients and companies.
               </p>
 
               <ul className="services_modal-services grid">
@@ -141,7 +168,9 @@ const Services = () => {
         <div className="services_content">
           <div>
             <i className="uil uil-edit services_icon"></i>
-            <h3 className="services_title"><span>Frontend </span> <br/> Developer</h3>
+            <h3 className="services_title">
+              <span>Frontend </span> <br /> Developer
+            </h3>
           </div>
 
           <span className="services_button" onClick={() => toggleTab(3)}>
@@ -149,14 +178,25 @@ const Services = () => {
             <i className="uil uil-arrow-right services_button-icon"></i>
           </span>
 
-          <div className={toggleState === 3 ? "services_modal active-modal" : "services_modal"}>
+          <div
+            className={
+              toggleState === 3
+                ? "services_modal active-modal"
+                : "services_modal"
+            }
+          >
             <div className="services_modal-content">
-              <i onClick={() => toggleTab(0)} className="uil uil-times services_modal-close"></i>
+              <i
+                onClick={() => toggleTab(0)}
+                className="uil uil-times services_modal-close"
+              ></i>
 
-              <h3 className="services_modal-title">Frontend <span>Developer</span></h3>
+              <h3 className="services_modal-title">
+                Frontend <span>Developer</span>
+              </h3>
               <p className="services_modal-description">
-                <span>2 years</span> of experienced service, delivering exceptional quality
-                to clients and companies.
+                <span>1 year</span> of experienced service, delivering
+                exceptional quality to clients and companies.
               </p>
 
               <ul className="services_modal-services grid">
---
+++ src/Components/qualifications/qualifications.jsx
@@ -0,0 +1,111 @@
+import React from "react";
+import "./qualifications.css";
+
+const qualifications = () => {
+  return (
+    <section className="qualification section">
+      <h2 className="section_title">Qualifications</h2>
+      <h4 className="section_subtitle">
+        My<span> Personel</span> Journey
+      </h4>
+
+      <div className="qualification_container container">
+        <div className="qualification_tabs">
+
+
+            <div className="qualification_button button--flex">
+                <i className="uil uil-graduation-cap qualification_active qualification_icon"></i>Education
+            </div>
+
+            <div className="qualification_button button--flex">
+                <i className="uil uil-briefcase-alt qualification_icon"></i>Experience
+            </div>
+
+
+        </div>
+
+        <div className="qualification_sections">
+            <div className="qualification_content qualification_content-active">
+                <div className="qualification_data">
+                    <div>
+                        <h3 className="qualification_title">Software Engeneering</h3>
+
+                        <span className="qualification_subtitle">ALX Africa</span>
+
+                        <div className="qualification_calendar">
+                        <i className="uil uil-calendar-alt"></i>AUG 2023 - present
+                        </div>
+                    </div>
+
+                    <div>
+                        <span className="qualification_rounder"></span>
+
+                        <span className="qualification_line"></span>
+                    </div>
+                </div>
+
+                <div className="qualification_data">
+                    <div></div>
+
+                    <div>
+                        <span className="qualification_rounder"></span>
+
+                        <span className="qualification_line"></span>
+                    </div>
+
+                    <div>
+                        <h3 className="qualification_title">Volunteer Front-End Engineer</h3>
+
+                        <span className="qualification_subtitle">iHealth and Wellness Foundation</span>
+
+                        <div className="qualification_calendar">
+                        <i className="uil uil-calendar-alt"></i>Jul 2023 - Present 
+                        </div>
+                    </div>
+                </div>
+
+                <div className="qualification_data">
+                    <div>
+                        <h3 className="qualification_title">Junior Web Developer</h3>
+
+                        <span className="qualification_subtitle">100Devs</span>
+
+                        <div className="qualification_calendar">
+                        <i className="uil uil-calendar-alt"></i>Nov 2022 - May 2023
+                        </div>
+                    </div>
+
+                    <div>
+                        <span className="qualification_rounder"></span>
+
+                        <span className="qualification_line"></span>
+                    </div>
+                </div>
+
+                {/* <div className="qualification_data">
+                    <div></div>
+
+                    <div>
+                        <span className="qualification_rounder"></span>
+
+                        <span className="qualification_line"></span>
+                    </div>
+
+                    <div>
+                        <h3 className="qualification_title">Frontend Developer</h3>
+
+                        <span className="qualification_subtitle">Jomo Kenyatta University of technology</span>
+
+                        <div className="qualification_calendar">
+                        <i className="uil uil-calendar-alt"></i>2021 - 2022
+                        </div>
+                    </div>
+                </div> */}
+            </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default qualifications;
