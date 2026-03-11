0c9cabfafec7563703cd9e4bebe29b7097e38d5e
Kelvin <kelvin@vemtry.so>
2026-03-11 10:39:50 +0300

    fixing

--- src/vite.config.js
+++ src/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+    assetsInclude: ['**/*.JPG', '**/*.jpg', '**/*.png', '**/*.PNG'], 
   build: {
     outDir: 'dist',
   },
