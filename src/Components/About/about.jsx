076da5c18229edaa1608903356bf1dfaa21aa77d
Kelvin <kelvin@vemtry.so>
2026-03-11 09:53:40 +0300

    feat: update About component and add CV, modify image imports, and enhance index file structure

--- src/Components/About/about.jsx
+++ src/Components/About/about.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import "./about.css";
-import AboutImg from "../../Assets/images/BID_0015.JPG";
-import Resume from "../../Assets/KELVIN-NJUIRI-RESUME.pdf";
+import AboutImg from "../../Assets/images/BID_0015.jpg";
+import Resume from "../../Assets/KELVIN-NJUIRI-CV.pdf";
 import Info from "./Info";
 import { motion } from "framer-motion";
 
